4774645803f4c99a059b8ba67c3e3d26b6febd6c
Juliano Resuli <jou1993@gmail.com>
2021-01-23 12:24:07 +0200

    creation of the player

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ pygame.display.set_caption("Space Invaders")
 icon=pygame.image.load("ufo.png")
 pygame.display.set_icon(icon)
 
+#player
+playerImg=pygame.image.load("player.png")
+playerX=370
+playerY=480
+
+def player(x,y):
+    screen.blit(playerImg,(x,y))
 
 
 
@@ -15,7 +22,11 @@ pygame.display.set_icon(icon)
 #Game Loop
 running=True
 while running:
+    screen.fill(((0,0,0)))
     for event in pygame.event.get():
 
         if event.type == pygame.QUIT:
             running=False
+
+        player(playerX,playerY)
+        pygame.display.update()
