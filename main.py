daeea2d40298f6e8c5ea3a9575d0e084cf2a1dd5
Juliano Resuli <jou1993@gmail.com>
2021-01-23 12:13:58 +0200

    create screen and game loop

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 import  pygame,sys
-import math
+
+# Create the screen
+screen=pygame.display.set_mode((800,600))
+
+
+
+
+
+
+
+#Game Loop
+running=True
+while running:
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            running=False
