2ac4d9b535ab8b1584b9079256d69a4ebd10a359
Juliano Resuli <jou1993@gmail.com>
2021-01-23 12:32:27 +0200

    added movement to player

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ pygame.display.set_icon(icon)
 playerImg=pygame.image.load("player.png")
 playerX=370
 playerY=480
+playerX_change=0
 
 def player(x,y):
     screen.blit(playerImg,(x,y))
@@ -27,6 +28,19 @@ while running:
 
         if event.type == pygame.QUIT:
             running=False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change -= 0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change += 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
 
-        player(playerX,playerY)
-        pygame.display.update()
+
+    playerX += playerX_change
+
+
+
+    player(playerX,playerY)
+    pygame.display.update()
